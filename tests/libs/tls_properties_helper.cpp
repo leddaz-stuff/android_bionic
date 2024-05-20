05ce8e886ea9e7685f897478ed1d031ee7442524
Elliott Hughes <enh@google.com>
2024-05-20 16:24:34 +0000

    tls_properties_helper: fix bionic check.
    __BIONIC__ is defined by <features.h>, not the toolchain.
    
    Change-Id: I202f42ec0f623ee39b2922d921ac07261994072c

--- tests/libs/tls_properties_helper.cpp
+++ tests/libs/tls_properties_helper.cpp
@@ -26,21 +26,12 @@
  * SUCH DAMAGE.
  */
 
-// Prevent tests from being compiled with glibc because thread_properties.h
-// only exists in Bionic.
-#if defined(__BIONIC__)
-
-#include <sys/thread_properties.h>
-
-#include <assert.h>
 #include <dlfcn.h>
 #include <elf.h>
 #include <err.h>
-#include <errno.h>
 #include <fcntl.h>
 #include <sched.h>
 #include <stdio.h>
-#include <string.h>
 #include <sys/prctl.h>
 #include <sys/ptrace.h>
 #include <sys/uio.h>
@@ -48,21 +39,27 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include "CHECK.h"
+
+#if __has_include(<sys/thread_properties.h>)
+
+#include <sys/thread_properties.h>
+
 // Helper binary to use TLS-related functions in thread_properties
 
 // Tests __get_static_tls_bound.
-thread_local int local_var;
+thread_local int thread_local_var;
 void test_static_tls_bounds() {
-  local_var = 123;
+  thread_local_var = 123;
   void* start_addr = nullptr;
   void* end_addr = nullptr;
 
   __libc_get_static_tls_bounds(reinterpret_cast<void**>(&start_addr),
                                reinterpret_cast<void**>(&end_addr));
-  assert(start_addr != nullptr);
-  assert(end_addr != nullptr);
+  CHECK(start_addr != nullptr);
+  CHECK(end_addr != nullptr);
 
-  assert(&local_var >= start_addr && &local_var < end_addr);
+  CHECK(&thread_local_var >= start_addr && &thread_local_var < end_addr);
 
   printf("done_get_static_tls_bounds\n");
 }
@@ -76,13 +73,13 @@ int found_count = 0;
 void test_iter_tls() {
   void* lib = dlopen("libtest_elftls_dynamic.so", RTLD_LOCAL | RTLD_NOW);
   large_tls_var[1025] = 'a';
-  auto cb = +[](void* dtls_begin, void* dtls_end, size_t dso_id, void* arg) {
+  auto cb = +[](void* dtls_begin, void* dtls_end, size_t /*dso_id*/, void* /*arg*/) {
     if (&large_tls_var >= dtls_begin && &large_tls_var < dtls_end) ++found_count;
   };
   __libc_iterate_dynamic_tls(gettid(), cb, nullptr);
 
   // It should be found exactly once.
-  assert(found_count == 1);
+  CHECK(found_count == 1);
   printf("done_iterate_dynamic_tls\n");
 }
 
@@ -93,24 +90,24 @@ void test_iterate_another_thread_tls() {
   found_count = 0;
 
   pid_t pid = fork();
-  assert(pid != -1);
+  CHECK(pid != -1);
   int status;
   if (pid) {
     // Parent.
-    assert(pid == wait(&status));
-    assert(0 == status);
+    CHECK(wait(&status) == pid);
+    CHECK(status == 0);
   } else {
     // Child.
     pid_t parent_pid = getppid();
-    assert(0 == ptrace(PTRACE_ATTACH, parent_pid));
-    assert(parent_pid == waitpid(parent_pid, &status, 0));
+    CHECK(ptrace(PTRACE_ATTACH, parent_pid) == 0);
+    CHECK(waitpid(parent_pid, &status, 0) == parent_pid);
 
-    auto cb = +[](void* dtls_begin, void* dtls_end, size_t dso_id, void* arg) {
+    auto cb = +[](void* dtls_begin, void* dtls_end, size_t /*dso_id*/, void* /*arg*/) {
       if (parent_addr >= dtls_begin && parent_addr < dtls_end) ++found_count;
     };
     __libc_iterate_dynamic_tls(parent_pid, cb, nullptr);
     // It should be found exactly once.
-    assert(found_count == 1);
+    CHECK(found_count == 1);
     printf("done_iterate_another_thread_tls\n");
   }
 }
@@ -122,7 +119,10 @@ int main() {
 }
 
 #else
+
 int main() {
-  return 0;
+  printf("test binary built without <sys/thread_properties.h>");
+  return 1;
 }
-#endif  // __BIONIC__
+
+#endif
