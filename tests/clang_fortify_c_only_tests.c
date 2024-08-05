8fe7847b629a8cc753cb9c11eb1f4d745adb2390
George Burgess IV <gbiv@google.com>
2024-08-05 13:02:51 -0600

    fortify: rewrite strlen to fold to a constant
    This new spelling allows Clang, when parsing C, to fold `strlen("foo")`
    into a constant in many more places.
    
    This shouldn't be much of a performance diff, since LLVM is already
    fully capable of folding `@__strlen_chk` into `@strlen` or a constant as
    appropriate. Mostly it matters for places where constants may be
    required, or may result in materially different codegen (e.g., static
    initializers, array bounds, ...)
    
    Bug: 139143453
    Test: mma
    Change-Id: Ib1636402a11338f6e05a731cc5a59c919cca09e8

---
+++ tests/clang_fortify_c_only_tests.c
@@ -0,0 +1,14 @@
+#ifndef __clang__
+#error "These tests are Clang-only."
+#endif
+
+#include <string.h>
+
+// This is a test specifically of bionic's FORTIFY machinery. Other stdlibs need not apply.
+#ifdef __BIONIC__
+
+// Ensure that strlen can be evaluated at compile-time. Clang doesn't support
+// this in C++, but does in C.
+_Static_assert(strlen("foo") == 3, "");
+
+#endif  // __BIONIC__
