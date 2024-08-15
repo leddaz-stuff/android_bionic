60ee237c5356aff0f5f957733e1f9546ff951c63
Elliott Hughes <enh@google.com>
2024-08-15 16:17:56 +0000

    <dlfcn.h>: track POSIX 2024.
    See https://www.austingroupbugs.net/view.php?id=1847 for background on the Dl_info vs Dl_info_t mixup.
    
    Change-Id: I1e7c90f8e58d0d065e028e8edc5606f8ddc5c83c

--- libc/include/dlfcn.h
+++ libc/include/dlfcn.h
@@ -35,6 +35,9 @@ __BEGIN_DECLS
 
 /**
  * dladdr() returns information using this structure.
+ * `Dl_info` is the traditional name for this,
+ * and thus the more portable choice.
+ * POSIX accidentally standardized `Dl_info_t` instead in 2024.
  */
 typedef struct {
   /** Pathname of the shared object that contains the given address. */
@@ -45,7 +48,7 @@ typedef struct {
   const char* _Nullable dli_sname;
   /** Exact address of the symbol named in `dli_sname`. */
   void* _Nullable dli_saddr;
-} Dl_info;
+} Dl_info, Dl_info_t;
 
 /**
  * [dlopen(3)](https://man7.org/linux/man-pages/man3/dlopen.3.html)
